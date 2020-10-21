a0cbab82445dcd770333fd545b21ccc299c717ac
Chulino <chulino@Chulinoui-MacBook.local>
2020-10-21 15:11:43 +0900

    fix : before install Hook 추가
    기존 빌드파일 삭제 후 write

---
+++ execute-before-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -d /home/ec2-user/deploy/swpp2020-team16/coding-mbti/build ]; then
+  sudo rm -rf /home/ec2-user/deploy/swpp2020-team16/coding-mbti/build
+fi
+
+sudo mkdir /home/ec2-user/deploy/swpp2020-team16/coding-mbti/build 

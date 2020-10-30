9e3b90676b18f31a6056d9f3c8142299bf0cc188
Chulino <chulino@Chulinoui-MacBook.local>
2020-10-30 19:02:24 +0900

    debug : travis.yml

--- coding-mbti/backend/local-docker/local-docker-tester-back/init-test.sh
+++ coding-mbti/backend/local-docker/local-docker-tester-back/init-test.sh
@@ -29,6 +29,27 @@ fi
 
 containerId=`docker ps | grep django-backend-server | awk '{print $1}'`
 
-echo "Container[ID=$containerId] is the django-backend-server. Testing..."
+echo "Container[ID=$containerId] is the django-backend-server. Checking installation completed..."
+
+DIR=$(dirname "$0")
+cd "$DIR"/../../
+
+currentInstallation=`docker exec "$containerId" pip list | grep -v "pip" | grep -v "setuptools" | grep -v "wheel" |awk 'NR>2{print $1}' | sort`
+currentCNT=`echo "$currentInstallation" |wc -l`
+havetoInstallation=`cat requirements.txt| awk '{print $1}' | sed 's/==*[0-9].*[0-9]//g' | sort`
+havetoCNT=`echo "$havetoInstallation"   |wc -l`
+
+until test "$currentInstallation" = "$havetoInstallation"; do
+    printf "%d out of %d installed.\n" "$currentCNT" "$havetoCNT"
+    currentInstallation=`docker exec "$containerId" pip list | grep -v "pip" | grep -v "setuptools" | grep -v "wheel" |awk 'NR>2{print $1}' | sort`
+    currentCNT=`echo "$currentInstallation" |wc -l`
+    echo "not every requirements are installed yet..."
+    echo "wait 10 secs..."
+    sleep 10
+done
+
+test "$currentInstallation" = "$havetoInstallation" && echo "[installation check completed.]"
+
+echo "test & coverage check..."
 
 docker  exec -it "$containerId" /bin/bash -c "cd /deploy/swpp2020-team16/coding-mbti/backend; coverage run --source='.' manage.py test"

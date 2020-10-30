41da5e82b1167d0b6e723e240be899cbc2871968
Dongmin Kim <dmk98@snu.ac.kr>
2020-10-30 15:37:03 +0900

    WIP: merge to dev branch

--- coding-mbti/backend/user/models.py
+++ coding-mbti/backend/user/models.py
@@ -5,7 +5,6 @@ from group.models import Group
 # Create your models here.
 class CodingStyle(models.Model):
     class Style(models.IntegerChoices):
-<<<<<<< HEAD
         # User Frienly - Machine Efficiency
         # Time Complexity - Intutive Code
         # Easy style - Formatted Style 
@@ -32,35 +31,11 @@ class CodingStyle(models.Model):
     EF_value = models.FloatField()
     JC_value = models.FloatField()
 
-=======
-        ISTJ = 1
-        ISTP = 2
-        ISFJ = 3
-        ISFP = 4
-        INTF = 5
-        INTP = 6
-        INFJ = 7
-        INFP = 8
-        ESTJ = 9
-        ESTP = 10
-        ESFJ = 11
-        ESFP = 12
-        ENTJ = 13
-        ENTP = 14
-        ENFJ = 15
-        ENFP = 16
-    style = models.IntegerField(choices=Style.choices)
->>>>>>> 017d4a65474014b6d3199ea2a5cd410dbd3d6fc7
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
     def create_user(self, username, password, email, salt, role):
-<<<<<<< HEAD
-=======
-        if not email :            
-            raise ValueError('must have user email')
->>>>>>> 017d4a65474014b6d3199ea2a5cd410dbd3d6fc7
         user = self.model(username=username,
             password=password,
             email=self.normalize_email(email),
@@ -77,17 +52,11 @@ class User(AbstractBaseUser):
 
     username = models.CharField(max_length=21, unique=True, default=None)
     email = models.CharField(max_length=190, unique=True, null=True, default=None)
-<<<<<<< HEAD
     nickname = models.CharField(max_length=21, unique=True)
     password = models.TextField(null=True, default=None)
     salt = models.TextField(null=True, default=None)
     role = models.IntegerField(choices=Role.choices)
     profile_img = models.ImageField()
-=======
-    password = models.TextField(null=True, default=None)
-    salt = models.TextField(null=True, default=None)
-    role = models.IntegerField(choices=Role.choices)
->>>>>>> 017d4a65474014b6d3199ea2a5cd410dbd3d6fc7
 
     objects = UserManager()
 

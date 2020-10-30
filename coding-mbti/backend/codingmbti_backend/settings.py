7f0579e9c23debc7b889574d955d560d565595a1
Chulino <chulino@Chulinoui-MacBook.local>
2020-10-30 20:12:42 +0900

    feat : update gitignore, add local docker initiator
    - updated gitignore
      - ignore coverage files(front,back)
      - ignore __pycache__ correctly
      - ignore .vscode/settings.json
    
    - added local docker initiator
      - backend initiator
      - backend testor
      - frontend docker initiator
        - 시간이 오래걸려서 사용하지 말 것을 추천
    
    - updated Dockerfile
      - backend dockerfile
    
    - updated travis.yml
      - dockerize testing

--- coding-mbti/backend/codingmbti_backend/settings.py
+++ coding-mbti/backend/codingmbti_backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'jh2x#17e#s9n*di%#dc%iqjmme8f+92bae58o2%#y$)eal8jh-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG')
+DEBUG = os.environ.get('DJANGO_DEBUG', True)
 
 ALLOWED_HOSTS = ['*']
 
@@ -76,7 +76,11 @@ WSGI_APPLICATION = 'codingmbti_backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+   'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DJANGO_DB_HOST'),
         'NAME': os.environ.get('DJANGO_DB_NAME'),

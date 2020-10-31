1a7caa97c92534ceda83e2a1d9af285fff5d7bc1
권순빈 <sunbin1696@gmail.com>
2020-10-31 18:00:58 +0900

    feat: implement home testing completed Navbar, Showprob, Footer, Home, and WEB IDE

--- coding-mbti/src/App.js
+++ coding-mbti/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './App.css';
+import Home from './containers/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Switch />
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
 }
-
 export default App;

320236d83004d74c23e45c021fb39e085c29d1eb
Dongmin Kim <dmk98@snu.ac.kr>
2020-11-10 03:10:57 +0900

    refactor: fix showprob test

--- coding-mbti/src/components/Showprob.test.js
+++ coding-mbti/src/components/Showprob.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
+import configureStore from '../configureStore';
 import Showprob from './Showprob';
 
 const { store } = configureStore();

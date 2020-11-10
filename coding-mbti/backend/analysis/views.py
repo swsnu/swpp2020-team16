90cf151cdabfeda7d3eaca87af5a09958db08016
권순빈 <sunbin1696@gmail.com>
2020-11-10 18:20:06 +0900

    implementing user report

--- coding-mbti/backend/analysis/views.py
+++ coding-mbti/backend/analysis/views.py
@@ -1,10 +1,32 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseBadRequest, HttpResponseNotAllowed, JsonResponse
 
-from analysis.models import SolutionReport
+from analysis.models import SolutionReport, UserReport
 
+def user_report(request):
+    if request.method == 'POST':
+        try :
+            solution1 = SolutionReport.objects.filter(author__id=request.user.id,title="ITP1_6_B_report").first().content
+            solution2 = SolutionReport.objects.filter(author__id=request.user.id,title="ITP2_3_B_report").first().content
+        except ObjectDoesNotExist as error:
+            return HttpResponseBadRequest(error)
+        try:
+            user_report = UserReport(author=request.user, solution1=solution1, solution2=solution2)
+        except (KeyError, JSONDecodeError) as error:
+            return HttpResponseBadRequest(error)
+        return HttpResponse(status=204)
 
-def provide_analysis_result(request, report_id):
+    else if request.method == 'GET':
+        try :
+            return JsonResponse(UserReport.objects.filter(request.user.id).first().to_dict(), status=200, safe=False)
+        else :
+            return HttpResponseBadRequest()
+    else :
+        return HttpResponseNotAllowed(['UPDATE', 'DELETE'])
+
+    
+
+def single_report(request, report_id):
     if request.method == 'GET':
         try:
             report = SolutionReport.objects.get(pk=report_id)

67781b6556725ecded75de3fb819c447d28a3cd6
Dongmin Kim <dmk98@snu.ac.kr>
2020-11-10 17:17:23 +0900

    fix: update Home.test.js with dependency

--- coding-mbti/src/containers/Home.test.js
+++ coding-mbti/src/containers/Home.test.js
@@ -1,16 +1,42 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 import Home from './Home';
+import configureStore from '../configureStore';
+
+const { store } = configureStore();
+const theme = createMuiTheme();
 
 describe('<Home/>', () => {
   let home;
 
   beforeEach(() => {
-    home = <Home />;
+    const options = {
+      // you can also just use 'bottom center'
+      position: positions.BOTTOM_CENTER,
+      timeout: 5000,
+      offset: '30px',
+      // you can also just use 'scale'
+      transition: transitions.SCALE,
+    };
+
+    home =
+      (
+        <Provider store={store}>
+          <ThemeProvider theme={theme}>
+            <AlertProvider template={AlertTemplate} {...options}>
+              <Home />
+            </AlertProvider>
+          </ThemeProvider>
+        </Provider>
+      );
   });
 
   it('should render without any error', () => {
-    const component = shallow(home);
+    const component = mount(home);
 
     const wrapper = component.find('.phrase');
     expect(wrapper.length).toBe(1);
@@ -20,7 +46,7 @@ describe('<Home/>', () => {
     delete window.location;
     window.location = { replace: jest.fn() };
 
-    const component = shallow(home);
+    const component = mount(home);
     const wrapper = component.find('#getTested');
     wrapper.simulate('click');
     expect(window.location.replace).toHaveBeenCalledTimes(1);

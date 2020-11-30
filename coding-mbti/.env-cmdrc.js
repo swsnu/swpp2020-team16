a9a5a479437a1687f5e07a2505944553cd7d9858
Chulino <chulino@Chulinoui-MacBook.local>
2020-11-30 19:05:12 +0900

    fix error

--- coding-mbti/.env-cmdrc.js
+++ coding-mbti/.env-cmdrc.js
@@ -3,9 +3,9 @@ module.exports = Promise.resolve({
     "REACT_APP_BACKEND_URL": "http://localhost:8000/api/",
   },
   "dev": {
-    "REACT_APP_BACKEND_URL": "https://codingmbti-dev.shop/api/",
+    "REACT_APP_BACKEND_URL": "http://ec2-54-146-103-181.compute-1.amazonaws.com/api/",
   },
   "prd": {
-    "REACT_APP_BACKEND_URL": "https://codingmbti.shop/api/",
+    "REACT_APP_BACKEND_URL": "http://ec2-3-218-239-17.compute-1.amazonaws.com/api/",
   }
 });

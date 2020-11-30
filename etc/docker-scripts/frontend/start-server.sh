25ba19386cd4f61e470c1ccd0627ec7c8f73c41d
Chulino <chulino@Chulinoui-MacBook.local>
2020-11-30 17:16:02 +0900

    fix : frontend code-deploy scripts

--- etc/docker-scripts/frontend/start-server.sh
+++ etc/docker-scripts/frontend/start-server.sh
@@ -1,34 +1,8 @@
 #!/bin/bash
   
 cd /deploy/swpp2020-team16/coding-mbti
-yarn install
+yarn
 yarn global add serve
 
-
-#AWS Specific code
-if sudo yum list | grep -q cloud-utils; then
-    echo "cloud-utils exists."
-    echo "ec2metadata command can be executed."
-else
-    echo "cloud-utils does not exist."
-    echo "ec2metadata command can not executed."
-    echo "installing cloud-utils..."
-    sudo yum install cloud-utils
-    echo "installation completed."
-fi
-
-CURRENT_EC2_INSTANCE_ID=$(ec2metadata --instance-id)
-DEV_EC2_INSTANCE_ID="i-02507b371f1a6ca12"
-PRD_EC2_INSTANCE_ID="i-0c21396ac5ea6c3b2"
-
-if [ "$CURRENT_EC2_INSTANCE_ID" == "$DEV_EC2_INSTANCE_ID" ]; then
-    yarn run:built:dev
-elif [ "$CURRENT_EC2_INSTANCE_ID" == "$PRD_EC2_INSTANCE_ID" ]; then
-    yarn run:built:prd
-else
-    echo "You've come to unreachable code."
-    echo "exiting..."
-    exit 1
-fi
-
-
+# `yarn run:built:dev` or `yarn run:built:prd` will be added dynamically based on ec2 instance id
+# handled in swpp2020-team16/aws-codedeploy-scripts/frontend/execute-before-install.sh

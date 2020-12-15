ca6fea51ec7a54e5dd5a4624d1874d595f66b83d
Chulino <chulino@Chulinoui-MacBook.local>
2020-12-16 07:07:35 +0900

    fix : static to nonstatic

--- coding-mbti/src/containers/StyleGrid.js
+++ coding-mbti/src/containers/StyleGrid.js
@@ -1,9 +1,10 @@
+/* eslint-disable class-methods-use-this */
 import React from 'react';
 import PropTypes from 'prop-types';
 import StyleGridComponent from '../components/StyleGrid';
 
 export default class StyleGrid extends React.Component {
-  static handleClick(pid, style) {
+  handleClick(pid, style) {
     window.location.replace(`../${pid}/${style}`);
   }
 
